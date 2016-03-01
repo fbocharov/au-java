b63875fe63ccd472c83ed6a7dd540e5712509328
Fyodor Bocharov <bocharovfedor@gmail.com>
2016-03-01 20:10:34 +0300

    Minor style checks.

--- hw/Trie/src/ru/spbau/bocharov/Trie/TrieImpl.java
+++ hw/Trie/src/ru/spbau/bocharov/Trie/TrieImpl.java
@@ -8,69 +8,71 @@ import java.util.Map;
  */
 public class TrieImpl implements Trie {
 
-    private class Node {
-        int subtreeSize_ = 0;
-        boolean isWordEnding_ = false;
-        Map<Character, Node> children_ = new HashMap<>();
+    private static class Node {
+        private int m_subtreeSize = 0;
+        private boolean m_isWordEnding = false;
+        private final Map<Character, Node> m_children = new HashMap<>();
     }
 
-    private Node root_ = new Node();
+    private final Node m_root = new Node();
 
     @Override
     public boolean add(String element) {
-        if (contains(element))
-            return false;
-        Node node = root_;
-        for (int i = 0; i < element.length(); ++i) {
-            char c = element.charAt(i);
-            if (!node.children_.containsKey(c)) {
-                node.children_.put(c, new Node());
-            }
-            node.subtreeSize_++;
-            node = node.children_.get(c);
-        }
-        if (node.isWordEnding_) {
+        if (contains(element)) {
             return false;
         }
 
-        node.isWordEnding_ = true;
-        node.subtreeSize_++;
+        Node node = m_root;
+        for (int i = 0; i < element.length(); i++) {
+            char c = element.charAt(i);
+            if (!node.m_children.containsKey(c)) {
+                node.m_children.put(c, new Node());
+            }
+            node.m_subtreeSize++;
+            node = node.m_children.get(c);
+        }
+        if (node.m_isWordEnding) {
+            return false;
+        }
+
+        node.m_isWordEnding = true;
+        node.m_subtreeSize++;
 
         return true;
     }
 
     @Override
     public boolean contains(String element) {
-        Node node = root_;
-        for (int i = 0; i < element.length(); ++i) {
+        Node node = m_root;
+        for (int i = 0; i < element.length(); i++) {
             char c = element.charAt(i);
-            if (!node.children_.containsKey(c)) {
+            if (!node.m_children.containsKey(c)) {
                 return false;
             }
-            node = node.children_.get(c);
+            node = node.m_children.get(c);
         }
-        return node.isWordEnding_;
+        return node.m_isWordEnding;
     }
 
     private boolean removeRecursive(Node node, String element, int pos) {
         if (pos == element.length()) {
-            node.subtreeSize_--;
-            boolean wordEnding = node.isWordEnding_;
-            node.isWordEnding_ = false;
+            node.m_subtreeSize--;
+            boolean wordEnding = node.m_isWordEnding;
+            node.m_isWordEnding = false;
             return wordEnding;
         }
 
         char c = element.charAt(pos);
-        if (!node.children_.containsKey(c)) {
+        if (!node.m_children.containsKey(c)) {
             return false;
         }
 
-        Node child = node.children_.get(c);
+        Node child = node.m_children.get(c);
         boolean contained = removeRecursive(child, element, pos + 1);
         if (contained) {
-            node.subtreeSize_--;
-            if (child.children_.isEmpty() && !child.isWordEnding_) {
-                node.children_.remove(c);
+            node.m_subtreeSize--;
+            if (child.m_children.isEmpty() && !child.m_isWordEnding) {
+                node.m_children.remove(c);
             }
         }
 
@@ -79,24 +81,24 @@ public class TrieImpl implements Trie {
 
     @Override
     public boolean remove(String element) {
-        return removeRecursive(root_, element, 0);
+        return removeRecursive(m_root, element, 0);
     }
 
     @Override
     public int size() {
-        return root_.subtreeSize_;
+        return m_root.m_subtreeSize;
     }
 
     @Override
     public int howManyStartsWithPrefix(String prefix) {
-        Node node = root_;
-        for (int i = 0; i < prefix.length(); ++i) {
+        Node node = m_root;
+        for (int i = 0; i < prefix.length(); i++) {
             char c = prefix.charAt(i);
-            if (!node.children_.containsKey(c)) {
+            if (!node.m_children.containsKey(c)) {
                 return 0;
             }
-            node = node.children_.get(c);
+            node = node.m_children.get(c);
         }
-        return node.subtreeSize_;
+        return node.m_subtreeSize;
     }
 }
