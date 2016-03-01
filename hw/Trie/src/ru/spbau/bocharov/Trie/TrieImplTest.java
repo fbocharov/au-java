c2b72412c83da8914df40d6d7590cdfb4f14c91d
Fyodor Bocharov <bocharovfedor@gmail.com>
2016-03-01 20:10:41 +0300

    Add more tests.

--- hw/Trie/src/ru/spbau/bocharov/Trie/TrieImplTest.java
+++ hw/Trie/src/ru/spbau/bocharov/Trie/TrieImplTest.java
@@ -32,7 +32,8 @@ public class TrieImplTest {
         assertFalse(trie.contains("H"));
         assertFalse(trie.contains("He"));
 
-        trie.add("");
+        assertTrue(trie.add(""));
+        assertFalse(trie.add(""));
 
         assertEquals(4, trie.size());
         assertEquals(4, trie.howManyStartsWithPrefix(""));
@@ -64,6 +65,17 @@ public class TrieImplTest {
         assertTrue(trie2.contains("BBD"));
         assertTrue(trie2.contains("BBE"));
         assertEquals(3, trie2.howManyStartsWithPrefix("BB"));
+
+        String str1 = "cat";
+        String str2 = "dog";
+
+        assertTrue(trie.add(str1));
+        assertTrue(trie.add(str2));
+        assertFalse(trie.add(str1));
+        assertFalse(trie.add(str2));
+
+        assertTrue(trie.add("ca"));
+        assertTrue(trie.add("c"));
     }
 
     @org.junit.Test
@@ -90,6 +102,35 @@ public class TrieImplTest {
         trie.remove("Fizz");
         assertTrue(trie.contains("FizzBuzz"));
         assertFalse(trie.contains("Fizz"));
+
+        trie.add("Very");
+        trie.add("VeryVery");
+        trie.add("VeryVeryLong");
+        trie.add("VeryVeryLongWord");
+
+        assertTrue(trie.contains("Very"));
+        assertTrue(trie.contains("VeryVery"));
+        assertTrue(trie.contains("VeryVeryLong"));
+        assertTrue(trie.contains("VeryVeryLongWord"));
+
+        assertFalse(trie.contains("AnyWord"));
+        assertFalse(trie.contains("Word"));
+        assertFalse(trie.contains("Ve"));
+        assertFalse(trie.contains("VeryV"));
+        assertFalse(trie.contains("VeryVeryLongWord1"));
+        assertFalse(trie.contains("VeryVeryLongWor"));
+
+        trie.remove("Very");
+        assertFalse(trie.contains("Very"));
+        assertTrue(trie.contains("VeryVery"));
+
+        trie.remove("VeryVery");
+        assertFalse(trie.contains("VeryVery"));
+        assertTrue(trie.contains("VeryVeryLong"));
+
+        trie.add("VeryVeryLongWord1");
+        trie.remove("VeryVeryLongWord");
+        assertTrue(trie.contains("VeryVeryLongWord1"));
     }
 
     @org.junit.Test
@@ -116,5 +157,63 @@ public class TrieImplTest {
         assertTrue(trie.remove("HI"));
 
         assertEquals(1, trie.howManyStartsWithPrefix("H"));
+
+        trie.add("String");
+        trie.add("Word");
+        assertFalse(trie.remove("YetWord"));
+        assertTrue(trie.remove("Word"));
+
+        trie.add("Word");
+        trie.add("word");
+
+        assertTrue(trie.remove("word"));
+    }
+
+    @org.junit.Test
+    public void testSize() throws Exception {
+        Trie trie = new TrieImpl();
+        assertEquals(trie.size(), 0);
+        trie.add("String");
+        assertEquals(trie.size(), 1);
+        trie.add("String2");
+        assertEquals(trie.size(), 2);
+        trie.add("Word");
+        assertEquals(trie.size(), 3);
+
+        trie.remove("str");
+        assertEquals(trie.size(), 3);
+
+        trie.remove("String");
+        assertEquals(trie.size(), 2);
+
+        trie.remove("String2");
+        assertEquals(trie.size(), 1);
+
+        trie.remove("Word");
+        assertEquals(trie.size(), 0);
+
+        trie.remove("Word");
+        assertEquals(trie.size(), 0);
+
+        trie.remove("Word");
+        assertEquals(trie.size(), 0);
+    }
+
+    @org.junit.Test
+    public void testHowManyStartsWithPrefix() throws Exception {
+        Trie trie = new TrieImpl();
+        trie.add("Very");
+        trie.add("VeryVery");
+        trie.add("VeryVeryLong");
+        trie.add("VeryVeryLongWord");
+
+        assertEquals(trie.howManyStartsWithPrefix("V"), 4);
+        assertEquals(trie.howManyStartsWithPrefix("Very"), 4);
+        assertEquals(trie.howManyStartsWithPrefix("VE"), 0);
+        assertEquals(trie.howManyStartsWithPrefix("VeryV"), 3);
+        assertEquals(trie.howManyStartsWithPrefix("VeryVery"), 3);
+        assertEquals(trie.howManyStartsWithPrefix("VeryVeryV"), 0);
+        assertEquals(trie.howManyStartsWithPrefix("VeryVeryL"), 2);
+        assertEquals(trie.howManyStartsWithPrefix("VeryVeryLongWord"), 1);
     }
 }
